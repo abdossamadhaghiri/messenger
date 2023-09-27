60c325c9c8bbdeea797f8616691fc4b95f9de96a
abdossamadhaghiri <abdossamadhaghiri@gmail.com>
2023-09-27 19:48:21 +0330

    add ApiAddresses.java in server for URLs in tests

---
+++ server/src/main/java/com/example/server/ApiAddresses.java
@@ -0,0 +1,14 @@
+package com.example.server;
+
+public class ApiAddresses {
+
+    public static final String HOST = "http://localhost:";
+    public static final String SIGN_UP_API_URL = "/signUp";
+    public static final String SIGN_IN_API_URL =  "/signIn/";
+    public static final String NEW_MESSAGE_API_URL = "/messages";
+    public static final String NEW_PV_API_URL =  "/chat";
+    public static final String NEW_GROUP_API_URL = "/groups";
+    public static final String DELETE_MESSAGE_API_URL = "/messages/";
+    public static final String EDIT_MESSAGE_API_URL = "/messages/";
+    public static final String GET_MESSAGE_API_URL = "/messages/";
+}
--- server/src/test/java/com/example/server/ServerControllerTest.java
+++ server/src/test/java/com/example/server/ServerControllerTest.java
@@ -22,6 +22,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.example.server.ApiAddresses.HOST;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -83,7 +84,7 @@ class ServerControllerTest {
 
     @Test
     void testSignUp_newUsername() {
-        String URL = "http://localhost:" + port + "/signUp";
+        String URL = HOST + port + ApiAddresses.SIGN_UP_API_URL;
         String newUsername = "amin";
         client.post().uri(URL).bodyValue(newUsername).exchange().expectStatus().isOk();
         assertTrue(userRepository.existsById(newUsername));
@@ -92,31 +93,31 @@ class ServerControllerTest {
     @Test
     void testSignUp_duplicateUsername() {
         userRepository.save(new User("amin", ServerController.generateToken()));
-        String URL = "http://localhost:" + port + "/signUp";
+        String url = HOST + port + ApiAddresses.SIGN_UP_API_URL;
         String duplicateUsername = "amin";
-        client.post().uri(URL).bodyValue(duplicateUsername).exchange().expectStatus().isBadRequest();
+        client.post().uri(url).bodyValue(duplicateUsername).exchange().expectStatus().isBadRequest();
     }
 
     @Test
     void testSignIn_newUsername() {
         String newUsername = "alireza";
-        String URL = "http://localhost:" + port + "/signIn/" + newUsername;
-        client.get().uri(URL).exchange().expectStatus().isBadRequest();
+        String url = HOST + port + ApiAddresses.SIGN_IN_API_URL + newUsername;
+        client.get().uri(url).exchange().expectStatus().isBadRequest();
     }
 
     @Test
     void testSignIn_duplicateUsername() {
         userRepository.save(new User("alireza", ServerController.generateToken()));
         String duplicateUsername = "alireza";
-        String URL = "http://localhost:" + port + "/signIn/" + duplicateUsername;
-        client.get().uri(URL).exchange().expectStatus().isOk();
+        String url = HOST + port + ApiAddresses.SIGN_IN_API_URL + duplicateUsername;
+        client.get().uri(url).exchange().expectStatus().isOk();
     }
 
     @Test
     void testNewMessage_invalidSender() {
         List<Chat> chats = chatRepository.findAll();
         MessageModel messageModel = new MessageModel(4L, "hello javad!", "alireza", chats.get(1).getId());
-        String url = "http://localhost:" + port + "/messages";
+        String url = HOST + port + ApiAddresses.NEW_MESSAGE_API_URL;
         client.post().uri(url).bodyValue(messageModel).exchange().expectStatus().isBadRequest().expectBody(String.class).consumeWith(result ->
                 assertEquals(Commands.INVALID_MESSAGE_CONTENT, result.getResponseBody()));
     }
@@ -125,7 +126,7 @@ class ServerControllerTest {
     void testNewMessage_chatDoesntExists() {
         List<Chat> chats = chatRepository.findAll();
         MessageModel messageModel = new MessageModel(4L, "hello javad!", "ali", chats.get(2).getId() + 1);
-        String url = "http://localhost:" + port + "/messages";
+        String url = HOST + port + ApiAddresses.NEW_MESSAGE_API_URL;
         client.post().uri(url).bodyValue(messageModel).exchange().expectStatus().isBadRequest().expectBody(String.class).consumeWith(result ->
                 assertEquals(Commands.INVALID_MESSAGE_CONTENT, result.getResponseBody()));
     }
@@ -135,16 +136,16 @@ class ServerControllerTest {
         List<Chat> chats = chatRepository.findAll();
         List<Message> messages = messageRepository.findAll();
         MessageModel messageModel = new MessageModel(((long) messages.get(2).getId()) + 1, "hello javad!", "ali", chats.get(1).getId());
-        String url = "http://localhost:" + port + "/messages";
+        String url = HOST + port + ApiAddresses.NEW_MESSAGE_API_URL;
         client.post().uri(url).bodyValue(messageModel).exchange().expectStatus().isOk();
-        Message message = new Message( messages.get(2).getId() + 1, messageModel.getText(), messageModel.getSender(), messageModel.getChatId());
+        Message message = new Message(messages.get(2).getId() + 1, messageModel.getText(), messageModel.getSender(), messageModel.getChatId());
         assertThat(message).isIn(messageRepository.findAll());
     }
 
     @Test
     void testNewPv_invalidFirstUsername() {
         PvModel pvModel = new PvModel("alireza", "ali");
-        String url = "http://localhost:" + port + "/chat";
+        String url = HOST + port + ApiAddresses.NEW_PV_API_URL;
         client.post().uri(url).bodyValue(pvModel).exchange().expectStatus().isBadRequest().expectBody(String.class).consumeWith(result ->
                 assertEquals(Commands.USERNAME_DOESNT_EXIST, result.getResponseBody()));
     }
@@ -152,15 +153,15 @@ class ServerControllerTest {
     @Test
     void testNewPv_invalidSecondUsername() {
         PvModel pvModel = new PvModel("ali", "alireza");
-        String url = "http://localhost:" + port + "/chat";
+        String url = HOST + port + ApiAddresses.NEW_PV_API_URL;
         client.post().uri(url).bodyValue(pvModel).exchange().expectStatus().isBadRequest().expectBody(String.class).consumeWith(result ->
                 assertEquals(Commands.USERNAME_DOESNT_EXIST, result.getResponseBody()));
     }
 
     @Test
     void testNewPv_duplicateChat() {
-        PvModel pvModel= new PvModel("ali", "reza");
-        String url = "http://localhost:" + port + "/chat";
+        PvModel pvModel = new PvModel("ali", "reza");
+        String url = HOST + port + ApiAddresses.NEW_PV_API_URL;
         client.post().uri(url).bodyValue(pvModel).exchange().expectStatus().isBadRequest().expectBody(String.class).consumeWith(result ->
                 assertEquals(Commands.CHAT_ALREADY_EXISTS, result.getResponseBody()));
     }
@@ -168,7 +169,7 @@ class ServerControllerTest {
     @Test
     void testNewPv_ok() {
         PvModel pvModel = new PvModel(chatRepository.findAll().get(2).getId() + 1, "ali", "amir");
-        String url = "http://localhost:" + port + "/chat";
+        String url = HOST + port + ApiAddresses.NEW_PV_API_URL;
         client.post().uri(url).bodyValue(pvModel).exchange().expectStatus().isOk();
         Pv pv = new Pv(pvModel.getId(), pvModel.getFirst(), pvModel.getSecond());
         assertThat(pv).isIn(chatRepository.findAll());
@@ -177,7 +178,7 @@ class ServerControllerTest {
     @Test
     void testNewGroup_invalidContent() {
         GroupModel groupModel = new GroupModel("ali", new ArrayList<>(List.of("ali", "reza", "alireza")), "groupName");
-        String url = "http://localhost:" + port + "/groups";
+        String url = HOST + port + ApiAddresses.NEW_GROUP_API_URL;
         client.post().uri(url).bodyValue(groupModel).exchange().expectStatus().isBadRequest().expectBody(String.class).consumeWith(result ->
                 assertEquals(Commands.INVALID_GROUP_CONTENT, result.getResponseBody()));
     }
@@ -186,7 +187,7 @@ class ServerControllerTest {
     void testNewGroup_ok() {
         GroupModel groupModel = new GroupModel(chatRepository.findAll().get(2).getId() + 1, "ali",
                 new ArrayList<>(List.of("ali", "reza", "javad")), "groupName");
-        String url = "http://localhost:" + port + "/groups";
+        String url = HOST + port + ApiAddresses.NEW_GROUP_API_URL;
         client.post().uri(url).bodyValue(groupModel).exchange().expectStatus().isOk();
         Group group = new Group(groupModel.getId(), groupModel.getOwner(), groupModel.getMembers(), groupModel.getName());
         assertThat(group).isIn(chatRepository.findAll());
@@ -195,7 +196,7 @@ class ServerControllerTest {
     @Test
     void testDeleteMessage_messageDoesntExists() {
         Message message = messageRepository.findAll().get(2);
-        String url = "http://localhost:" + port + "/messages/" + (message.getId() + 1);
+        String url = HOST + port + ApiAddresses.DELETE_MESSAGE_API_URL + (message.getId() + 1);
         client.delete().uri(url).header(HttpHeaders.AUTHORIZATION, userRepository.findById(message.getSender()).get().getToken()).exchange().expectStatus().isBadRequest().expectBody(String.class).consumeWith(result ->
                 assertEquals(Commands.MESSAGE_DOESNT_EXIST, result.getResponseBody()));
     }
@@ -203,7 +204,7 @@ class ServerControllerTest {
     @Test
     void testDeleteMessage_ok() {
         Message message = messageRepository.findAll().get(2);
-        String url = "http://localhost:" + port + "/messages/" + message.getId();
+        String url = HOST + port + ApiAddresses.DELETE_MESSAGE_API_URL + message.getId();
         client.delete().uri(url).header(HttpHeaders.AUTHORIZATION, userRepository.findById(message.getSender()).get().getToken()).exchange().expectStatus().isOk();
         assertThat(messageRepository.findById((long) message.getId())).isEmpty();
     }
@@ -212,17 +213,17 @@ class ServerControllerTest {
     void testEditMessage_messageDoesntExists() {
         Message message = messageRepository.findAll().get(2);
         Message newMessage = new Message("edited text!", message.getSender(), message.getChatId());
-        String url = "http://localhost:" + port + "/messages/" + (message.getId() + 1);
+        String url = HOST + port + ApiAddresses.EDIT_MESSAGE_API_URL + (message.getId() + 1);
         client.put().uri(url).header(HttpHeaders.AUTHORIZATION, userRepository.findById(message.getSender()).get().getToken())
                 .bodyValue(newMessage).exchange().expectStatus().isBadRequest().expectBody(String.class).consumeWith(result ->
-                assertEquals(Commands.MESSAGE_DOESNT_EXIST, result.getResponseBody()));
+                        assertEquals(Commands.MESSAGE_DOESNT_EXIST, result.getResponseBody()));
     }
 
     @Test
     void testEditMessage_ok() {
         Message message = messageRepository.findAll().get(2);
         Message newMessage = new Message("edited text!", message.getSender(), message.getChatId());
-        String url = "http://localhost:" + port + "/messages/" + message.getId();
+        String url = HOST + port + ApiAddresses.EDIT_MESSAGE_API_URL + message.getId();
         client.put().uri(url).header(HttpHeaders.AUTHORIZATION, userRepository.findById(message.getSender()).get().getToken())
                 .bodyValue(newMessage).exchange().expectStatus().isOk();
         assertEquals(newMessage.getText(), messageRepository.findById(message.getId()).get().getText());
@@ -231,7 +232,7 @@ class ServerControllerTest {
     @Test
     void testGetMessage_invalidId() {
         Message message = messageRepository.findAll().get(2);
-        String url = "http://localhost:" + port + "/messages/" + (message.getId() + 1);
+        String url = HOST + port + ApiAddresses.GET_MESSAGE_API_URL + (message.getId() + 1);
         client.get().uri(url).header(HttpHeaders.AUTHORIZATION, userRepository.findById(message.getSender()).get().getToken()).exchange().expectStatus().isBadRequest();
     }
 
@@ -239,10 +240,10 @@ class ServerControllerTest {
     void testGetMessage_ok() {
         Message message = messageRepository.findAll().get(2);
         MessageModel expected = message.createMessageModel();
-        String url = "http://localhost:" + port + "/messages/" + message.getId();
+        String url = HOST + port + ApiAddresses.GET_MESSAGE_API_URL + message.getId();
         client.get().uri(url).header(HttpHeaders.AUTHORIZATION, userRepository.findById(message.getSender()).get().getToken())
                 .exchange().expectStatus().isOk().expectBody(MessageModel.class).consumeWith(result ->
-                assertEquals(result.getResponseBody(), expected));
+                        assertEquals(result.getResponseBody(), expected));
     }
 
 
