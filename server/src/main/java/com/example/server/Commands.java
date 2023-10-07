46588cca3ac33366a2c9c3692107cf595710feeb
abdossamad <abdossamadhaghiri@gmail.com>
2023-10-07 14:08:02 +0330

    delete a command that never used in server commands

--- server/src/main/java/com/example/server/Commands.java
+++ server/src/main/java/com/example/server/Commands.java
@@ -11,7 +11,6 @@ public class Commands {
     public static final String REPLIED_MESSAGE_DOESNT_EXIST = "the replied message doesnt exist!";
     public static final String REPLIED_MESSAGE_IS_NOT_IN_THIS_CHAT = "the replied message isn't in this chat!";
     public static final String START_YOUR_CHAT = "start your chat";
-    public static final String INVALID_MESSAGE_CONTENT = "invalid message content!";
     public static final String SENT = "sent!";
     public static final String INVALID_GROUP_CONTENT = "invalid group content!";
     public static final String GROUP_SUCCESSFULLY_CREATED = "the group successfully created!";
